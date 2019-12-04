8dd7a9e42a317fca6e8577bbb0fd90398ecc23e3
ilknurozcan <54802982+ilknurozcan@users.noreply.github.com>
2019-12-04 22:02:02 +0300

    Sign Up Second Page

---
+++ app/src/main/java/com/example/hocapp/SignUpActivitySecondPage.java
@@ -0,0 +1,14 @@
+package com.example.hocapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivitySecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_second_page);
+    }
+}
