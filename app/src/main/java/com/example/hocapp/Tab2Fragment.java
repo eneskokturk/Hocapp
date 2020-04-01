ea36c657a7d8e18010c224be56cbf4694d1010a1
Enes KÖKTÜRK <47602411+eneskokturk@users.noreply.github.com>
2020-04-01 22:27:49 +0300

    Update Tab2Fragment.java

--- app/src/main/java/com/example/hocapp/Tab2Fragment.java
+++ app/src/main/java/com/example/hocapp/Tab2Fragment.java
@@ -16,26 +16,10 @@ import android.widget.TextView;
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class Tab2Fragment extends Fragment {
-
-
-    private static final String TAG="İlanListele";
-
-    private PageViewModel pageViewModel;
-
-    public Tab2Fragment(){
-
-    }
-
-    public static Tab1Fragment newInstance(){
-        return new Tab1Fragment();
-    }
-
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
-        pageViewModel= ViewModelProviders.of(this).get(PageViewModel.class);
-        pageViewModel.setIndex(TAG);
+    public Tab2Fragment() {
+        // Required empty public constructor
     }
 
 
@@ -43,16 +27,7 @@ public class Tab2Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root=inflater.inflate(R.layout.fragment_home,container,false);
-
-        final TextView textView = root.findViewById(R.id.section_label);
-        pageViewModel.getText().observe(this,new Observer<String>(){
-
-            public void onChanged(String s){
-                textView.setText(s);
-            }
-        });
-        return root;
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
     }
 
 }
